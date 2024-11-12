39cfd2ef03bfc9d728a8d75ccf9ecc490de1b852
Bill Hallahan <whallahan@binhamton.edu>
2024-11-12 02:05:38 -0500

    List comps

--- GHC/Enum.hs
+++ GHC/Enum.hs
@@ -540,78 +540,75 @@ eftIntFB = eftIntFB
 -- -- The code is more complicated because of worries about Int overflow.
 -- 
 -- -- See Note [How the Enum rules work]
-{-# RULES
-"efdtInt"       [~1] forall x1 x2 y.
-                     efdtInt x1 x2 y = build (\ c n -> efdtIntFB c n x1 x2 y)
-"efdtIntUpList" [1]  efdtIntFB (:) [] = efdtInt
- #-}
+-- {-# RULES
+-- "efdtInt"       [~1] forall x1 x2 y.
+--                      efdtInt x1 x2 y = build (\ c n -> efdtIntFB c n x1 x2 y)
+-- "efdtIntUpList" [1]  efdtIntFB (:) [] = efdtInt
+--  #-}
 -- 
 efdInt :: Int# -> Int# -> [Int]
-efdInt = efdInt
--- -- [x1,x2..maxInt]
--- efdInt x1 x2
---  | isTrue# (x2 >=# x1) = case maxInt of I# y -> efdtIntUp x1 x2 y
---  | otherwise           = case minInt of I# y -> efdtIntDn x1 x2 y
--- 
+-- [x1,x2..maxInt]
+efdInt x1 x2
+ | isTrue# (x2 >=# x1) = case maxInt of I# y -> efdtIntUp x1 x2 y
+ | otherwise           = case minInt of I# y -> efdtIntDn x1 x2 y
+
 {-# NOINLINE [1] efdtInt #-}
 efdtInt :: Int# -> Int# -> Int# -> [Int]
-efdtInt = efdtInt
--- -- [x1,x2..y]
--- efdtInt x1 x2 y
---  | isTrue# (x2 >=# x1) = efdtIntUp x1 x2 y
---  | otherwise           = efdtIntDn x1 x2 y
+-- [x1,x2..y]
+efdtInt x1 x2 y
+ | isTrue# (x2 >=# x1) = efdtIntUp x1 x2 y
+ | otherwise           = efdtIntDn x1 x2 y
 -- 
-{-# INLINE [0] efdtIntFB #-}
-efdtIntFB :: (Int -> r -> r) -> r -> Int# -> Int# -> Int# -> r
-efdtIntFB = efdtIntFB
+-- {-# INLINE [0] efdtIntFB #-}
+-- efdtIntFB :: (Int -> r -> r) -> r -> Int# -> Int# -> Int# -> r
 -- efdtIntFB c n x1 x2 y
 --  | isTrue# (x2 >=# x1) = efdtIntUpFB c n x1 x2 y
 --  | otherwise           = efdtIntDnFB c n x1 x2 y
--- 
--- -- Requires x2 >= x1
--- efdtIntUp :: Int# -> Int# -> Int# -> [Int]
--- efdtIntUp x1 x2 y    -- Be careful about overflow!
---  | isTrue# (y <# x2) = if isTrue# (y <# x1) then [] else [I# x1]
---  | otherwise = -- Common case: x1 <= x2 <= y
---                let !delta = x2 -# x1 -- >= 0
---                    !y' = y -# delta  -- x1 <= y' <= y; hence y' is representable
--- 
---                    -- Invariant: x <= y
---                    -- Note that: z <= y' => z + delta won't overflow
---                    -- so we are guaranteed not to overflow if/when we recurse
---                    go_up x | isTrue# (x ># y') = [I# x]
---                            | otherwise         = I# x : go_up (x +# delta)
---                in I# x1 : go_up x2
--- 
--- -- Requires x2 >= x1
+
+-- Requires x2 >= x1
+efdtIntUp :: Int# -> Int# -> Int# -> [Int]
+efdtIntUp x1 x2 y    -- Be careful about overflow!
+ | isTrue# (y <# x2) = if isTrue# (y <# x1) then [] else [I# x1]
+ | otherwise = -- Common case: x1 <= x2 <= y
+               let !delta = x2 -# x1 -- >= 0
+                   !y' = y -# delta  -- x1 <= y' <= y; hence y' is representable
+
+                   -- Invariant: x <= y
+                   -- Note that: z <= y' => z + delta won't overflow
+                   -- so we are guaranteed not to overflow if/when we recurse
+                   go_up x | isTrue# (x ># y') = [I# x]
+                           | otherwise         = I# x : go_up (x +# delta)
+               in I# x1 : go_up x2
+
+-- Requires x2 >= x1
 -- efdtIntUpFB :: (Int -> r -> r) -> r -> Int# -> Int# -> Int# -> r
 -- efdtIntUpFB c n x1 x2 y    -- Be careful about overflow!
 --  | isTrue# (y <# x2) = if isTrue# (y <# x1) then n else I# x1 `c` n
 --  | otherwise = -- Common case: x1 <= x2 <= y
 --                let !delta = x2 -# x1 -- >= 0
 --                    !y' = y -# delta  -- x1 <= y' <= y; hence y' is representable
--- 
+
 --                    -- Invariant: x <= y
 --                    -- Note that: z <= y' => z + delta won't overflow
 --                    -- so we are guaranteed not to overflow if/when we recurse
 --                    go_up x | isTrue# (x ># y') = I# x `c` n
 --                            | otherwise         = I# x `c` go_up (x +# delta)
 --                in I# x1 `c` go_up x2
--- 
--- -- Requires x2 <= x1
--- efdtIntDn :: Int# -> Int# -> Int# -> [Int]
--- efdtIntDn x1 x2 y    -- Be careful about underflow!
---  | isTrue# (y ># x2) = if isTrue# (y ># x1) then [] else [I# x1]
---  | otherwise = -- Common case: x1 >= x2 >= y
---                let !delta = x2 -# x1 -- <= 0
---                    !y' = y -# delta  -- y <= y' <= x1; hence y' is representable
--- 
---                    -- Invariant: x >= y
---                    -- Note that: z >= y' => z + delta won't underflow
---                    -- so we are guaranteed not to underflow if/when we recurse
---                    go_dn x | isTrue# (x <# y') = [I# x]
---                            | otherwise         = I# x : go_dn (x +# delta)
---    in I# x1 : go_dn x2
+
+-- Requires x2 <= x1
+efdtIntDn :: Int# -> Int# -> Int# -> [Int]
+efdtIntDn x1 x2 y    -- Be careful about underflow!
+ | isTrue# (y ># x2) = if isTrue# (y ># x1) then [] else [I# x1]
+ | otherwise = -- Common case: x1 >= x2 >= y
+               let !delta = x2 -# x1 -- <= 0
+                   !y' = y -# delta  -- y <= y' <= x1; hence y' is representable
+
+                   -- Invariant: x >= y
+                   -- Note that: z >= y' => z + delta won't underflow
+                   -- so we are guaranteed not to underflow if/when we recurse
+                   go_dn x | isTrue# (x <# y') = [I# x]
+                           | otherwise         = I# x : go_dn (x +# delta)
+   in I# x1 : go_dn x2
 -- 
 -- -- Requires x2 <= x1
 -- efdtIntDnFB :: (Int -> r -> r) -> r -> Int# -> Int# -> Int# -> r
