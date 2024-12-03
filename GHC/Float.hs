42473b08888795a03d050d338424649b4d5b6dfd
Bill Hallahan <whallahan@binhamton.edu>
2024-12-03 15:32:11 -0500

    More RealFloat functions

--- GHC/Float.hs
+++ GHC/Float.hs
@@ -138,20 +138,20 @@ class  (RealFrac a, Floating a) => RealFloat a  where
 --     -- | a constant function, returning the number of digits of
 --     -- 'floatRadix' in the significand
     floatDigits         :: a -> Int
---     -- | a constant function, returning the lowest and highest values
---     -- the exponent may assume
---     floatRange          :: a -> (Int,Int)
---     -- | The function 'decodeFloat' applied to a real floating-point
---     -- number returns the significand expressed as an 'Integer' and an
---     -- appropriately scaled exponent (an 'Int').  If @'decodeFloat' x@
---     -- yields @(m,n)@, then @x@ is equal in value to @m*b^^n@, where @b@
---     -- is the floating-point radix, and furthermore, either @m@ and @n@
---     -- are both zero or else @b^(d-1) <= 'abs' m < b^d@, where @d@ is
---     -- the value of @'floatDigits' x@.
---     -- In particular, @'decodeFloat' 0 = (0,0)@. If the type
---     -- contains a negative zero, also @'decodeFloat' (-0.0) = (0,0)@.
---     -- /The result of/ @'decodeFloat' x@ /is unspecified if either of/
---     -- @'isNaN' x@ /or/ @'isInfinite' x@ /is/ 'True'.
+    -- | a constant function, returning the lowest and highest values
+    -- the exponent may assume
+    floatRange          :: a -> (Int,Int)
+    -- | The function 'decodeFloat' applied to a real floating-point
+    -- number returns the significand expressed as an 'Integer' and an
+    -- appropriately scaled exponent (an 'Int').  If @'decodeFloat' x@
+    -- yields @(m,n)@, then @x@ is equal in value to @m*b^^n@, where @b@
+    -- is the floating-point radix, and furthermore, either @m@ and @n@
+    -- are both zero or else @b^(d-1) <= 'abs' m < b^d@, where @d@ is
+    -- the value of @'floatDigits' x@.
+    -- In particular, @'decodeFloat' 0 = (0,0)@. If the type
+    -- contains a negative zero, also @'decodeFloat' (-0.0) = (0,0)@.
+    -- /The result of/ @'decodeFloat' x@ /is unspecified if either of/
+    -- @'isNaN' x@ /or/ @'isInfinite' x@ /is/ 'True'.
     decodeFloat         :: a -> (Integer,Int)
     -- | 'encodeFloat' performs the inverse of 'decodeFloat' in the
     -- sense that for finite @x@ with the exception of @-0.0@,
@@ -161,21 +161,21 @@ class  (RealFrac a, Floating a) => RealFloat a  where
     -- occurs); usually the closer, but if @m@ contains too many bits,
     -- the result may be rounded in the wrong direction.
     encodeFloat         :: Integer -> Int -> a
---     -- | 'exponent' corresponds to the second component of 'decodeFloat'.
---     -- @'exponent' 0 = 0@ and for finite nonzero @x@,
---     -- @'exponent' x = snd ('decodeFloat' x) + 'floatDigits' x@.
---     -- If @x@ is a finite floating-point number, it is equal in value to
---     -- @'significand' x * b ^^ 'exponent' x@, where @b@ is the
---     -- floating-point radix.
---     -- The behaviour is unspecified on infinite or @NaN@ values.
+    -- | 'exponent' corresponds to the second component of 'decodeFloat'.
+    -- @'exponent' 0 = 0@ and for finite nonzero @x@,
+    -- @'exponent' x = snd ('decodeFloat' x) + 'floatDigits' x@.
+    -- If @x@ is a finite floating-point number, it is equal in value to
+    -- @'significand' x * b ^^ 'exponent' x@, where @b@ is the
+    -- floating-point radix.
+    -- The behaviour is unspecified on infinite or @NaN@ values.
     exponent            :: a -> Int
---     -- | The first component of 'decodeFloat', scaled to lie in the open
---     -- interval (@-1@,@1@), either @0.0@ or of absolute value @>= 1\/b@,
---     -- where @b@ is the floating-point radix.
---     -- The behaviour is unspecified on infinite or @NaN@ values.
---     significand         :: a -> a
---     -- | multiplies a floating-point number by an integer power of the radix
---     scaleFloat          :: Int -> a -> a
+    -- | The first component of 'decodeFloat', scaled to lie in the open
+    -- interval (@-1@,@1@), either @0.0@ or of absolute value @>= 1\/b@,
+    -- where @b@ is the floating-point radix.
+    -- The behaviour is unspecified on infinite or @NaN@ values.
+    significand         :: a -> a
+    -- | multiplies a floating-point number by an integer power of the radix
+    scaleFloat          :: Int -> a -> a
     -- | 'True' if the argument is an IEEE \"not-a-number\" (NaN) value
     isNaN               :: a -> Bool
     -- | 'True' if the argument is an IEEE infinity or negative infinity
@@ -199,29 +199,29 @@ class  (RealFrac a, Floating a) => RealFloat a  where
 --     atan2               :: a -> a -> a
 -- 
 -- 
---     exponent x          =  if m == 0 then 0 else n + floatDigits x
---                            where (m,n) = decodeFloat x
--- 
---     significand x       =  encodeFloat m (negate (floatDigits x))
---                            where (m,_) = decodeFloat x
--- 
---     scaleFloat 0 x      =  x
---     scaleFloat k x
---       | isFix           =  x
---       | otherwise       =  encodeFloat m (n + clamp b k)
---                            where (m,n) = decodeFloat x
---                                  (l,h) = floatRange x
---                                  d     = floatDigits x
---                                  b     = h - l + 4*d
---                                  -- n+k may overflow, which would lead
---                                  -- to wrong results, hence we clamp the
---                                  -- scaling parameter.
---                                  -- If n + k would be larger than h,
---                                  -- n + clamp b k must be too, simliar
---                                  -- for smaller than l - d.
---                                  -- Add a little extra to keep clear
---                                  -- from the boundary cases.
---                                  isFix = x == 0 || isNaN x || isInfinite x
+    exponent x          =  if m == Z# 0# then I# 0# else n + floatDigits x
+                           where (m,n) = decodeFloat x
+
+    significand x       =  encodeFloat m (negate (floatDigits x))
+                           where (m,_) = decodeFloat x
+
+    -- scaleFloat 0 x      =  x
+    -- scaleFloat k x
+    --   | isFix           =  x
+    --   | otherwise       =  encodeFloat m (n + clamp b k)
+    --                        where (m,n) = decodeFloat x
+    --                              (l,h) = floatRange x
+    --                              d     = floatDigits x
+    --                              b     = h - l + 4*d
+    --                              -- n+k may overflow, which would lead
+    --                              -- to wrong results, hence we clamp the
+    --                              -- scaling parameter.
+    --                              -- If n + k would be larger than h,
+    --                              -- n + clamp b k must be too, simliar
+    --                              -- for smaller than l - d.
+    --                              -- Add a little extra to keep clear
+    --                              -- from the boundary cases.
+    --                              isFix = x == 0 || isNaN x || isInfinite x
 -- 
 --     atan2 y x
 --       | x > 0            =  atan (y/x)
@@ -391,7 +391,7 @@ instance  Floating Float  where
 instance  RealFloat Float  where
 --     floatRadix _        =  FLT_RADIX        -- from float.h
     floatDigits _       =  FLT_MANT_DIG     -- ditto
---     floatRange _        =  (FLT_MIN_EXP, FLT_MAX_EXP) -- ditto
+    floatRange _        =  (FLT_MIN_EXP, FLT_MAX_EXP) -- ditto
 -- 
     decodeFloat (F# f) = case f `smtEqFloat#` 0.0# of
                             True -> (Z# 0#, I# 0#)
@@ -404,16 +404,16 @@ instance  RealFloat Float  where
     exponent x          = case decodeFloat x of
                             (m,n) -> if m == (Z# 0#) then (I# 0#) else n + floatDigits x
 -- 
---     significand x       = case decodeFloat x of
---                             (m,_) -> encodeFloat m (negate (floatDigits x))
--- 
---     scaleFloat 0 x      = x
---     scaleFloat k x
---       | isFix           = x
---       | otherwise       = case decodeFloat x of
---                             (m,n) -> encodeFloat m (n + clamp bf k)
---                         where bf = FLT_MAX_EXP - (FLT_MIN_EXP) + 4*FLT_MANT_DIG
---                               isFix = x == 0 || isFloatFinite x == 0
+    significand x       = case decodeFloat x of
+                            (m,_) -> encodeFloat m (negate (floatDigits x))
+
+    scaleFloat 0 x      = x
+    scaleFloat k x
+      | isFix           = x
+      | otherwise       = case decodeFloat x of
+                            (m,n) -> encodeFloat m (n + clamp bf k)
+                        where bf = FLT_MAX_EXP - (FLT_MIN_EXP) + 4*FLT_MANT_DIG
+                              isFix = x == F# 0.0# || isFloatFinite x == I# 0#
 -- 
     isNaN x          = (I# 0#) /= isFloatNaN x
     isInfinite x     = I# 0# /= isFloatInfinite x
@@ -574,7 +574,7 @@ instance  RealFrac Double  where
 instance  RealFloat Double  where
 --     floatRadix _        =  FLT_RADIX        -- from float.h
     floatDigits _       =  DBL_MANT_DIG     -- ditto
---     floatRange _        =  (DBL_MIN_EXP, DBL_MAX_EXP) -- ditto
+    floatRange _        =  (DBL_MIN_EXP, DBL_MAX_EXP) -- ditto
 -- 
     decodeFloat (D# f) = case f $==## 0.0## of
                             True -> (Z# 0#, I# 0#)
@@ -586,17 +586,17 @@ instance  RealFloat Double  where
     exponent x          = case decodeFloat x of
                             (m,n) -> if m == (Z# 0#) then (I# 0#) else n + floatDigits x
 
---     significand x       = case decodeFloat x of
---                             (m,_) -> encodeFloat m (negate (floatDigits x))
--- 
---     scaleFloat 0 x      = x
---     scaleFloat k x
---       | isFix           = x
---       | otherwise       = case decodeFloat x of
---                             (m,n) -> encodeFloat m (n + clamp bd k)
---                         where bd = DBL_MAX_EXP - (DBL_MIN_EXP) + 4*DBL_MANT_DIG
---                               isFix = x == 0 || isDoubleFinite x == 0
--- 
+    significand x       = case decodeFloat x of
+                            (m,_) -> encodeFloat m (negate (floatDigits x))
+
+    scaleFloat 0 x      = x
+    scaleFloat k x
+      | isFix           = x
+      | otherwise       = case decodeFloat x of
+                            (m,n) -> encodeFloat m (n + clamp bd k)
+                        where bd = DBL_MAX_EXP - (DBL_MIN_EXP) + 4*DBL_MANT_DIG
+                              isFix = x == D# 0.0## || isDoubleFinite x == I# 0#
+
     isNaN x             = I# 0# /= isDoubleNaN x
     isInfinite x        = I# 0# /= isDoubleInfinite x
     isDenormalized x    = I# 0# /= isDoubleDenormalized x
@@ -1236,6 +1236,9 @@ isFloatDenormalized (F# f) = if isFloatDenormalized# f then I# 1# else I# 0#
 isFloatNegativeZero :: Float -> Int
 isFloatNegativeZero (F# f) = if isFloatNegativeZero# f then I# 1# else I# 0#
 -- foreign import ccall unsafe "isFloatFinite" isFloatFinite :: Float -> Int
+isFloatFinite :: Float -> Int
+isFloatFinite (F# f) = if isFloatInfinite# f then I# 0# else I# 1#
+
 -- 
 -- foreign import ccall unsafe "isDoubleNaN" isDoubleNaN :: Double -> Int
 isDoubleNaN :: Double -> Int
@@ -1250,6 +1253,8 @@ isDoubleDenormalized (D# d) = if isDoubleDenormalized# d then I# 1# else I# 0#
 isDoubleNegativeZero :: Double -> Int
 isDoubleNegativeZero (D# d) = if isDoubleNegativeZero# d then I# 1# else I# 0#
 -- foreign import ccall unsafe "isDoubleFinite" isDoubleFinite :: Double -> Int
+isDoubleFinite :: Double -> Int
+isDoubleFinite (D# f) = if isDoubleInfinite# f then I# 0# else I# 1#
 -- 
 -- 
 -- ------------------------------------------------------------------------
@@ -1335,11 +1340,11 @@ showSignedFloat showPos p x
        = showParen (p > fromInteger (Z# 6#)) (showChar '-' . showPos (negate x))
    | otherwise = showPos x
 
--- {-
--- We need to prevent over/underflow of the exponent in encodeFloat when
--- called from scaleFloat, hence we clamp the scaling parameter.
--- We must have a large enough range to cover the maximum difference of
--- exponents returned by decodeFloat.
--- -}
--- clamp :: Int -> Int -> Int
--- clamp bd k = max (-bd) (min bd k)
+{-
+We need to prevent over/underflow of the exponent in encodeFloat when
+called from scaleFloat, hence we clamp the scaling parameter.
+We must have a large enough range to cover the maximum difference of
+exponents returned by decodeFloat.
+-}
+clamp :: Int -> Int -> Int
+clamp bd k = max (-bd) (min bd k)
