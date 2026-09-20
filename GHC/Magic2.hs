5f5f4b7ec7da7b105611d492ea9e5d91a743e407
Ryan Fanchiotti <ryan@rur.com>
2026-09-20 16:54:25 -0400

    fix runrw sig

--- GHC/Magic2.hs
+++ GHC/Magic2.hs
@@ -12,11 +12,12 @@ module GHC.Magic2
 
 -- import GHC.Magic
 import GHC.Prim2 (State#, realWorld#, RealWorld, Int#)
+import GHC.Types (RuntimeRep, TYPE)
 
 oneShot :: (a -> b) -> (a -> b)
 oneShot f = f
 
-runRW# :: forall r o.
+runRW# :: forall (r :: RuntimeRep) (o :: TYPE r).
           (State# RealWorld -> o) -> o
 -- See Note [runRW magic] in GHC.CoreToStg.Prep.
 {-# NOINLINE runRW# #-}  -- runRW# is inlined manually in CorePrep
