5db04f6d2f579e23f91e464276dca1497224e3fc
ryanfanchiotti <ryan@rur.com>
2026-09-21 00:43:14 -0400

    Fix type signature of runRW# (#66)
    * fix runrw sig
    
    * trying kindsignatures
    
    * polykinds

--- GHC/Magic2.hs
+++ GHC/Magic2.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PolyKinds #-}
 
 module GHC.Magic2
   ( module GHC.Magic2
@@ -12,11 +14,12 @@ module GHC.Magic2
 
 -- import GHC.Magic
 import GHC.Prim2 (State#, realWorld#, RealWorld, Int#)
+import GHC.Types (RuntimeRep, TYPE)
 
 oneShot :: (a -> b) -> (a -> b)
 oneShot f = f
 
-runRW# :: forall r o.
+runRW# :: forall (r :: RuntimeRep) (o :: TYPE r).
           (State# RealWorld -> o) -> o
 -- See Note [runRW magic] in GHC.CoreToStg.Prep.
 {-# NOINLINE runRW# #-}  -- runRW# is inlined manually in CorePrep
